e9108a6b80c19602ad08e3bf3360ba9e642db95b
GavinKatayama <gavin.katayama@ucalgary.ca>
2023-03-04 18:45:43 +0000

    -Code redundancy eliminated in getLifeExpectancy() method.

--- src/application/LifeExpectancy.java
+++ src/application/LifeExpectancy.java
@@ -1,10 +1,10 @@
 package application;
 
-	/** This class represents the life expectancy of the program user.
-	 * 
-	 * @author CS219-user
-	 *
-	 */
+/** This class represents the life expectancy of the program user.
+ * 
+ * @author CS219-user
+ *
+ */
 public class LifeExpectancy {
 	//Variables related to life expectancy defined here.
 	int currentAge;
@@ -21,7 +21,6 @@ public class LifeExpectancy {
     String duchenneMD;
     String rabies;
 	
-	//Creating a constructor for life expectancy.
     /** This constructor constructs a LifeExpectancy object with instance variables that keep track of
      * mortality-related information.
      * 
@@ -57,7 +56,6 @@ public class LifeExpectancy {
 		rabies = rabiesStatus;
 	}
 	
-	//Creating a method for calculating life expectancy.
 	/**
 	 * This method calculates your future lifetime in years based on your current age, gender,
 	 * smoking habits, and based on any terminal illnesses you have.
@@ -66,70 +64,73 @@ public class LifeExpectancy {
 	 */
 	int getLifeExpectancy() {
 		
-        //Life expectancy variable initialized to 0.
-        int futureLifetime = 0;
+		//Life expectancy variable initialized to 0.
+		int futureLifetime = 0;
 		
-		if (currentAge >= 15 && currentAge <= 19 && gender.equals("m"))
-        	futureLifetime = 65;
-        else if (currentAge >= 20 && currentAge <= 24 && gender.equals("m"))
-        	futureLifetime = 60;
-        else if (currentAge >= 25 && currentAge <= 29 && gender.equals("m"))
-        	futureLifetime = 56;
-        else if (currentAge >= 30 && currentAge <= 24 && gender.equals("m"))
-        	futureLifetime = 51;
-        else if (currentAge >= 35 && currentAge <= 39 && gender.equals("m"))
-        	futureLifetime = 46;
-        else if (currentAge >= 40 && currentAge <= 44 && gender.equals("m"))
-        	futureLifetime = 41;
-        else if (currentAge >= 45 && currentAge <= 49 && gender.equals("m"))
-        	futureLifetime = 37;
-        else if (currentAge >= 50 && currentAge <= 54 && gender.equals("m"))
-        	futureLifetime = 32;
-        else if (currentAge >= 55 && currentAge <= 59 && gender.equals("m"))
-        	futureLifetime = 28;
-        else if (currentAge >= 60 && currentAge <= 64 && gender.equals("m"))
-        	futureLifetime = 23;
-        else if (currentAge >= 65 && currentAge <= 69 && gender.equals("m"))
-        	futureLifetime = 19;
-        else if (currentAge >= 70 && currentAge <= 74 && gender.equals("m"))
-        	futureLifetime = 15;
-        else if (currentAge >= 75 && currentAge <= 79 && gender.equals("m"))
-        	futureLifetime = 12;
-        else if (currentAge >= 80 && currentAge <= 84 && gender.equals("m"))
-        	futureLifetime = 9;
-        else if (currentAge >= 85 && gender.equals("m"))
-        	futureLifetime = 6;	        
-        
-        else if (currentAge >= 15 && currentAge <= 19 && gender.equals("f"))
-        	futureLifetime = 69;
-        else if (currentAge >= 20 && currentAge <= 24 && gender.equals("f"))
-        	futureLifetime = 64;
-        else if (currentAge >= 25 && currentAge <= 29 && gender.equals("f"))
-        	futureLifetime = 59;
-        else if (currentAge >= 30 && currentAge <= 24 && gender.equals("f"))
-        	futureLifetime = 54;
-        else if (currentAge >= 35 && currentAge <= 39 && gender.equals("f"))
-        	futureLifetime = 49;
-        else if (currentAge >= 40 && currentAge <= 44 && gender.equals("f"))
-        	futureLifetime = 45;
-        else if (currentAge >= 45 && currentAge <= 49 && gender.equals("f"))
-        	futureLifetime = 40;
-        else if (currentAge >= 50 && currentAge <= 54 && gender.equals("f"))
-        	futureLifetime = 35;
-        else if (currentAge >= 55 && currentAge <= 59 && gender.equals("f"))
-        	futureLifetime = 30;
-        else if (currentAge >= 60 && currentAge <= 64 && gender.equals("f"))
-        	futureLifetime = 26;
-        else if (currentAge >= 65 && currentAge <= 69 && gender.equals("f"))
-        	futureLifetime = 22;
-        else if (currentAge >= 70 && currentAge <= 74 && gender.equals("f"))
-        	futureLifetime = 17;
-        else if (currentAge >= 75 && currentAge <= 79 && gender.equals("f"))
-        	futureLifetime = 13;
-        else if (currentAge >= 80 && currentAge <= 84 && gender.equals("f"))
-        	futureLifetime = 10;
-        else if (currentAge >= 85 && gender.equals("f"))
-        	futureLifetime = 7;
+		if (gender.equals("m")) {
+			if (currentAge <= 19)
+				futureLifetime = 65;
+			else if (currentAge <= 24)
+				futureLifetime = 60;
+			else if (currentAge <= 29)
+				futureLifetime = 56;
+			else if (currentAge <= 34)
+				futureLifetime = 51;
+			else if (currentAge <= 39)
+				futureLifetime = 46;
+			else if (currentAge <= 44)
+				futureLifetime = 41;
+			else if (currentAge <= 49)
+				futureLifetime = 37;
+			else if (currentAge <= 54)
+				futureLifetime = 32;
+			else if (currentAge <= 59)
+				futureLifetime = 28;
+			else if (currentAge <= 64)
+				futureLifetime = 23;
+			else if (currentAge <= 69)
+				futureLifetime = 19;
+			else if (currentAge <= 74)
+				futureLifetime = 15;
+			else if (currentAge <= 79)
+				futureLifetime = 12;
+			else if (currentAge <= 84)
+				futureLifetime = 9;
+			else
+				futureLifetime = 6;	
+		}
+		else {
+			if (currentAge <= 19)
+				futureLifetime = 69;
+			else if (currentAge <= 24)
+				futureLifetime = 64;
+			else if (currentAge <= 29)
+				futureLifetime = 59;
+			else if (currentAge <= 34)
+				futureLifetime = 54;
+			else if (currentAge <= 39)
+				futureLifetime = 49;
+			else if (currentAge <= 44)
+				futureLifetime = 45;
+			else if (currentAge <= 49)
+				futureLifetime = 40;
+			else if (currentAge <= 54)
+				futureLifetime = 35;
+			else if (currentAge <= 59)
+				futureLifetime = 30;
+			else if (currentAge <= 64)
+				futureLifetime = 26;
+			else if (currentAge <= 69)
+				futureLifetime = 22;
+			else if (currentAge <= 74)
+				futureLifetime = 17;
+			else if (currentAge <= 79)
+				futureLifetime = 13;
+			else if (currentAge <= 84)
+				futureLifetime = 10;
+			else
+				futureLifetime = 7;	
+		}
 		
 		//We are assuming that the user enters at most one terminal illness.
         if (alzheimers.equals("y"))
@@ -161,7 +162,7 @@ public class LifeExpectancy {
         	futureLifetime -= 9;
         else if (smokingHabits.equals("l"))
         	futureLifetime -= 5;
-        else if (smokingHabits.equals("n"))
+        else
         	futureLifetime -= 0;
         
         if (futureLifetime < 0)
