bcdb14c4c74bfa92a71e6b5c263ddc4678755e04
GavinKatayama <gavin.katayama@ucalgary.ca>
2023-03-17 01:22:17 +0000

    -Replaced an "if" with an "else if" in calculateAndGoToOutputScene().

--- src/application/LifeExpectancyCalculatorController.java
+++ src/application/LifeExpectancyCalculatorController.java
@@ -166,17 +166,17 @@ public class LifeExpectancyCalculatorController {
 	    	if (element.equals("Yes"))
 	    		numberOfYes += 1;
 	    }
+	    
 	    if (numberOfYes > 1) {
 	    	validNumberOfTerminalIllnesses = false;
 	    	outputMessage = "You may only select Yes for zero or one terminal illness.";
 	    } else if (Integer.parseInt(currentAgeTextField.getText()) < 65 && alzheimersContainer.getChoiceBox().getValue().equals("Yes")) {
 	    	validNumberOfTerminalIllnesses = false;
 	    	outputMessage = "You may not select Yes for Alzheimer's Disease if you are under 65.";
-	    }
-
-		if (validCurrentAge && validNumberOfTerminalIllnesses)
+	    } else if (validCurrentAge && validNumberOfTerminalIllnesses)
 			outputMessage = "Your life expectancy is " + lifeExpectancy.getLifeExpectancy() + " more years";
-		outputLabel.setText(outputMessage);
+		
+	    outputLabel.setText(outputMessage);
 		applicationStage.setScene(outputScene);
 	}
     
